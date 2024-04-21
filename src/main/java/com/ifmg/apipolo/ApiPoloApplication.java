cb4dd5e8afef237e64efb8b12eda647e3c12aa9d
pedromends <joaopedromend14@gmail.com>
2024-04-21 16:41:25 -0300

    Inicialized

---
+++ src/main/java/com/ifmg/apipolo/ApiPoloApplication.java
@@ -0,0 +1,13 @@
+package com.ifmg.apipolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPoloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifmg/apipolo/ApiPoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifmg.apipolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
