0584efa23cffbd01b543b6c68a2807323f61f9c0
pedromends <joaopedromend14@gmail.com>
2024-09-24 23:05:33 -0300

    upload pdf

--- src/main/java/com/ifmg/apipolo/controller/DocsController.java
+++ src/main/java/com/ifmg/apipolo/controller/DocsController.java
@@ -5,9 +5,18 @@ import com.ifmg.apipolo.service.EdictsService;
 import com.ifmg.apipolo.vo.DocsVO;
 import com.ifmg.apipolo.vo.EdictsVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @CrossOrigin("*")
 @RestController
@@ -18,9 +27,32 @@ public class DocsController {
     DocsService docsService;
 
     @PostMapping("/create")
-    public ResponseEntity<Object> createDocs(@RequestBody DocsVO docsVO) {
-        docsService.createDocs(docsVO);
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<String> createDocs(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Por favor, selecione um arquivo para upload.");
+        }
+
+        try {
+            // Define o caminho para a pasta de uploads
+            Path uploadsPath = Paths.get("C:\\ProgramData\\Kimeratech\\uploads");
+
+            // Obtenha o arquivo de destino
+            File uploadsDir = uploadsPath.toFile();
+
+            // Criar a pasta se ela não existir
+            if (!uploadsDir.exists()) {
+                uploadsDir.mkdirs();
+            }
+
+            // Define o caminho completo para o novo arquivo (com o nome do arquivo original)
+            Path filePath = uploadsPath.resolve(file.getOriginalFilename());
+
+            // Escreve o arquivo no diretório uploads
+            Files.write(filePath, file.getBytes());
+            return ResponseEntity.ok("Arquivo enviado com sucesso: " + file.getOriginalFilename());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Falha ao enviar o arquivo.");
+        }
     }
 
     @PutMapping("/update")
---
+++ src/main/java/com/ifmg/apipolo/service/FileStorageService.java
@@ -0,0 +1,7 @@
+package com.ifmg.apipolo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileStorageService {
+}
